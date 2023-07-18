6fcfdc23419aa845762699043cd633a0294fc402
samarth126 <samarth.bhole2001@gmail.com>
2023-07-18 22:11:24 +0530

    all styling

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("contact", views.contact, name="contact"),
+    path("services", views.services, name="services"),
 ]
--- base/views.py
+++ base/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def services(request):
+    return render(request, 'services.html')
